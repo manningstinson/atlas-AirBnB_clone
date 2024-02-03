214155c6d0499f3c7652097f78f22c753a22dc15
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-02-03 20:54:32 +0000

    updated base_model

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module containing the BaseModel class
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+    def __init__(self):
+        """
+        Initializes a new instance of BaseModel
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the instance
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
---
+++ tests/test_models/test_base.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Unittests for BaseModel class
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test cases for BaseModel class
+    """
+    def setUp(self):
+        """
+        Set up method to create an instance of BaseModel
+        """
+        self.my_model = BaseModel()
+
+    def test_attributes(self):
+        """
+        Test the attributes of BaseModel instance
+        """
+        self.assertTrue(hasattr(self.my_model, 'id'))
+        self.assertTrue(hasattr(self.my_model, 'created_at'))
+        self.assertTrue(hasattr(self.my_model, 'updated_at'))
+
+    def test_str_method(self):
+        """
+        Test the __str__ method of BaseModel
+        """
+        expected_str = "[BaseModel] ({}) {}".format(self.my_model.id, self.my_model.__dict__)
+        self.assertEqual(str(self.my_model), expected_str)
+
+    def test_save_method(self):
+        """
+        Test the save method of BaseModel
+        """
+        old_updated_at = self.my_model.updated_at
+        self.my_model.save()
+        self.assertNotEqual(old_updated_at, self.my_model.updated_at)
+
+    def test_to_dict_method(self):
+        """
+        Test the to_dict method of BaseModel
+        """
+        my_model_dict = self.my_model.to_dict()
+        self.assertTrue('__class__' in my_model_dict)
+        self.assertTrue('created_at' in my_model_dict)
+        self.assertTrue('updated_at' in my_model_dict)
+        self.assertEqual(my_model_dict['__class__'], 'BaseModel')
+
+if __name__ == '__main__':
+    unittest.main()
