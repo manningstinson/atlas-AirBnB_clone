25e32e51fa9847ea2785a0ddde43d39e75936925
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-04 21:28:37 +0000

    Updated Review Test

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,15 @@
+import unittest
+from models.review import Review
+
+class TestReview(unittest.TestCase):
+    def test_attributes(self):
+        review = Review()
+        self.assertTrue(hasattr(review, 'place_id'))
+        self.assertTrue(hasattr(review, 'user_id'))
+        self.assertTrue(hasattr(review, 'text'))
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+if __name__ == '__main__':
+    unittest.main()
