42b00530179ac70184697476d0221f0e9e3705d1
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-02-07 18:22:32 +0000

    updated user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """User class inheriting from BaseModel."""
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize User instance."""
         super().__init__(*args, **kwargs)
