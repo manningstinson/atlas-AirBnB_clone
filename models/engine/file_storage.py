2326f1a9a655f35af7823cac0c59f51aac10c6bd
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-02-10 08:47:02 +0000

    update engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines the FileStorage class."""
 import json
-from models.base_model import BaseModel, User, State, City, Place, Amenity, Review
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """
